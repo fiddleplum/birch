a8298fde3a87e417cd04594973085c1497aff1da
Stephen Hurley <stephenhurley@gmail.com>
2020-08-21 00:49:24 -0700

    Made model component add and destroy itself properly.

--- src/world/components/model_component.ts
+++ src/world/components/model_component.ts
@@ -1,11 +1,21 @@
 import { Component, Entity } from '../internal';
 import { Render } from '../../render/index';
 
+/** A simple component that contains an accessible model. */
 export class ModelComponent extends Component {
 	constructor(entity: Entity) {
 		super(entity);
 
+		// Create the model.
 		this._model = entity.world.engine.renderer.models.create();
+		// Add the model to the scene.
+		this.entity.world.scene.models.add(this._model);
+	}
+
+	/** Destroys the model component. */
+	destroy(): void {
+		this.entity.world.scene.models.remove(this._model);
+		this.engine.renderer.models.destroy(this._model);
 	}
 
 	/** Gets the model. */
@@ -13,10 +23,6 @@ export class ModelComponent extends Component {
 		return this._model;
 	}
 
+	/** The model. */
 	private _model: Render.Model;
-}
-
-export namespace ModelComponent {
-	export enum Events {
-	}
 }
