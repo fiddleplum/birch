3061b202d66fb3737bccc65d51dae10fcafe9934
Stephen Hurley <stephenhurley@gmail.com>
2020-09-13 08:54:43 -0700

    Fixed bug with frame-model system not updating on start.

--- src/world/systems/frame_model_system.ts
+++ src/world/systems/frame_model_system.ts
@@ -12,17 +12,22 @@ export class FrameModelSystem extends System {
 	processEvent(component: Component, event: symbol): void {
 		if (event === Entity.ComponentCreated) {
 			this.subscribeToEvents(component);
+			this._updateModels(component as FrameComponent);
 		}
 		else if (event === Component.ComponentDestroyed) {
 			this.unsubscribeFromEvents(component);
 		}
 		else {
-			const frameComponent = component as FrameComponent;
-			const modelComponents = component.entity.getAll(ModelComponent);
-			if (modelComponents !== undefined) {
-				for (const modelComponent of modelComponents) {
-					modelComponent.model.uniforms.setUniform('modelMatrix', frameComponent.localToWorld.array);
-				}
+			this._updateModels(component as FrameComponent);
+		}
+	}
+
+	/** Updates all of the models given a frame component. */
+	private _updateModels(frameComponent: FrameComponent): void {
+		const modelComponents = frameComponent.entity.getAll(ModelComponent);
+		if (modelComponents !== undefined) {
+			for (const modelComponent of modelComponents) {
+				modelComponent.model.uniforms.setUniform('modelMatrix', frameComponent.localToWorld.array);
 			}
 		}
 	}
