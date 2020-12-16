7b019f086c2ea39c9840c915c4f748afca3c6f80
Stephen Hurley <stephenhurley@gmail.com>
2020-12-15 23:06:05 -0800

    Added pool util class.

---
+++ src/utils/pool.ts
@@ -0,0 +1,56 @@
+/** Pool for keeping garbage production down. */
+export class Pool<Type extends { new(): Type; }> {
+	/** Constructor. */
+	constructor(type: Type) {
+		this._type = type;
+	}
+
+	/** Get an object from the pool. */
+	get(): Type {
+		if (this._numFreeIndices > 0) {
+			const object = this._objects[this._freeIndices[this._numFreeIndices - 1]];
+			this._numFreeIndices -= 1;
+			return object;
+		}
+		else {
+			const object = new this._type();
+			this._objectsToIndices.set(object, this._objects.length);
+			this._objects.push(object);
+			return object;
+		}
+	}
+
+	/** Release an object back to the pool. */
+	release(object: Type): void {
+		// Get the index of the object.
+		const index = this._objectsToIndices.get(object);
+		if (index === undefined) {
+			return;
+		}
+		this._objectsToIndices.delete(object);
+		if (this._numFreeIndices === this._freeIndices.length) {
+			this._freeIndices.push(index);
+		}
+		else {
+			this._freeIndices[this._numFreeIndices] = index;
+		}
+		this._numFreeIndices += 1;
+	}
+
+	/** The type of object that will be constructed. */
+	private _type: Type;
+
+	/** The list of objects, free and used. */
+	private _objects: Type[] = [];
+
+	/** The list of indices in _objects that are free. */
+	private _freeIndices: number[] = [];
+
+	/** The number of free indices in the _freeIndices array. */
+	private _numFreeIndices: number = 0;
+
+	/** A mapping of objects to their indices. */
+	private _objectsToIndices: Map<Type, number> = new Map();
+}
+
+export default Pool;
